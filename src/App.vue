621315bd8c45d049e1053b12909f96f52c824dd3
jianjie.fu <jianjie.fu@garena.com>
2023-02-22 19:37:07 +0800

    [dev] basic time clock

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="clock">
+    {{ dateStr }}<br/>
+    {{ timeStr }}
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue"
+
+const dateStr = ref("")
+const timeStr = ref("")
+
+setInterval(()=>{
+  const tempStr = (new Date().toLocaleString()).split(' ')
+  dateStr.value = tempStr[0]
+  timeStr.value = tempStr[1]
+}, 1000)
+
+</script>
+
+<style lang="scss" scoped>
+.clock {
+  font-size: 15vw;
+  text-align: center;
+}
+</style>
