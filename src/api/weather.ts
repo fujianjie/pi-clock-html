115865949147750cd2370efd232f975a90003e38
jianjie.fu <jianjie.fu@garena.com>
2024-02-25 16:51:04 +0800

    [dev] update api path

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 // https://yikeapi.com/ 易客云
 
-const weatherAPI = 'https://v0.yiketianqi.com/api?unescape=1&version=v61&appid=84241347&appsecret=nWT5wMWQ'
+const weatherAPI = 'https://v1.yiketianqi.com/api?unescape=1&version=v61&appid=84241347&appsecret=nWT5wMWQ'
 
 export function weatherApi() {
     return axios({
